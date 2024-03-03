8f3624cfa0f5fc8b317792dae9abfafd72adc434
andrewshipe <andrew.shipe@gmail.com>
2024-03-03 01:05:59 -0600

    refactor printing

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,9 @@ fn main() {
     println!("Hello, world!");
     println!("You say hello, I say goodbye")
 }
+
+fn print(m: &str) {
+    println!("{m}")
+}
 // this is a change to the main file
 // a new comment, wow!
